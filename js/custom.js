b10cfdd7f29f763664a2dd000bd490345645842a
Mabel Lin <ns05170131@gmail.com>
2021-11-24 23:52:39 +0800

    測試2

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,3 @@
-//load header/footer
-document.onSelectStart=function(){return false;};
-document.body.oncopy = function(){event.returnValue=false;}
-
-$(function () {
-	var includes = $('[data-include]')
-	$.each(includes, function () {
-	  var file = 'layout/' + $(this).data('include') + '.html'
-	  $(this).load(file)
-	})
-});
 
 //最新活動
 var event = new Vue({
