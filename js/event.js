98a2744d0bfcd39e6c236c433e07b16c29bc6ec4
Mabel Lin <52170396+FourMLin@users.noreply.github.com>
2021-11-28 11:40:23 +0800

    456

---
+++ js/event.js
@@ -0,0 +1,15 @@
+//最新活動
+var event = new Vue({
+    el: '#newEvent',
+    data: {
+        newevent: [{
+            title: '大河與小飛，鳳飛飛 紀念閩南語親子劇',
+            date: '2021.10.23',
+            category:'親子劇場',
+            link:'new-event-inner.html',
+            images:'img/blog/grid/grid-1.jpg'
+        },
+        ],
+
+    }
+});
